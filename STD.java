2ab84c401896eb527978e8e3a2a2ee9f0fd40fdf
Jahan Rashidi <56563402+ImNotJahan@users.noreply.github.com>
2021-09-05 17:38:45 -0700

    Added <String, String> support

--- STD.java
+++ STD.java
@@ -31,6 +31,15 @@ public class STD
         }};
         System.out.println(SIADictToString(siaDict));
         System.out.println(SIADictToString(SIAStringToDict(SIADictToString(siaDict))));
+
+        Map<String, String> ssDict = new HashMap<String, String>()
+        {{
+            put("first letter", "a");
+            put("5 words", "very very very very words");
+            put("three", "3");
+        }};
+        System.out.println(SSDictToString(ssDict));
+        System.out.println(SSDictToString(SSStringToDict(SSDictToString(ssDict))));
     }
 
 
@@ -65,6 +74,7 @@ public class STD
 
         return remappedDict;
     }
+
 
     /**
      * SIA = String, Integer Array
@@ -107,6 +117,39 @@ public class STD
             }
 
             remappedDict.put(splitKAV[0], integerArray);
+        }
+
+        return remappedDict;
+    }
+
+    /**
+     * SS = String, String
+     */
+    public static String SSDictToString(Map<String, String> dictionary)
+    {
+        StringBuilder stringedDict = new StringBuilder();
+        for(String key : dictionary.keySet())
+        {
+            stringedDict.append(key).append(":").append(dictionary.get(key)).append(",");
+        }
+
+        return stringedDict.substring(0, stringedDict.length() - 1);
+    }
+
+
+    /**
+     * SS = String, String
+     */
+    public static Map<String, String> SSStringToDict(String string)
+    {
+        Map<String, String> remappedDict = new HashMap<>();
+        String[] keysAndValues = string.split(",");
+
+        for(String keyAndValue : keysAndValues)
+        {
+            //KAV = Key and Value
+            String[] splitKAV = keyAndValue.split(":");
+            remappedDict.put(splitKAV[0], splitKAV[1]);
         }
 
         return remappedDict;
